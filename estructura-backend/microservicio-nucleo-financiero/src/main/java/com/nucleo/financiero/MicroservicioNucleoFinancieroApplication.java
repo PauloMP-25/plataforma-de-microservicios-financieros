753cf22f23fb3d18fe9f1ada2ab61ab657ea7d2e
Paulo Moron Poma <paulomoronpoma@gmail.com>
2026-04-13 17:40:17 -0500

    feat; Creando la estructura base de carpetas de cada paquete del microservicio-nucleo-financiero.

---
+++ estructura-backend/microservicio-nucleo-financiero/src/main/java/com/nucleo/financiero/MicroservicioNucleoFinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.nucleo.financiero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioNucleoFinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioNucleoFinancieroApplication.class, args);
+	}
+
+}
