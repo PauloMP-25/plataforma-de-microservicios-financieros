419fb0fa732832b9ff755cf4f9443006da294128
Paulo Moron Poma <paulomoronpoma@gmail.com>
2026-04-17 20:03:44 -0500

    feat(cliente); Corrigiendo errores de ramificacion y merge

--- estructura-backend/microservicio-cliente/src/main/java/com/cliente/infraestructura/configuracion/ConfiguracionGeneral.java
+++ estructura-backend/microservicio-cliente/src/main/java/com/cliente/infraestructura/configuracion/ConfiguracionGeneral.java
@@ -1,4 +1,4 @@
-package com.usuario.infraestructura.configuracion;
+package com.cliente.infraestructura.configuracion;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
