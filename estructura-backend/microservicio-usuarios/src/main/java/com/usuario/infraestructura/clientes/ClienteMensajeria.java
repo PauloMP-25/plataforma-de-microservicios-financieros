1b3fbddb04250283827c64f67070f830218cc058
Paulo Moron Poma <paulomoronpoma@gmail.com>
2026-04-20 14:50:24 -0500

    fix: Agregando el puente de comunicación entre el microservicio usuario y mensajeria asi como la documentación respectiva.

--- estructura-backend/microservicio-usuarios/src/main/java/com/usuario/infraestructura/clientes/ClienteMensajeria.java
+++ estructura-backend/microservicio-usuarios/src/main/java/com/usuario/infraestructura/clientes/ClienteMensajeria.java
@@ -1,9 +1,12 @@
 package com.usuario.infraestructura.clientes;
 
 import com.usuario.aplicacion.dtos.SolicitudGenerarOtp;
+import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Feign Client que invoca el endpoint de activación de cuenta en el
@@ -24,9 +27,17 @@ public interface ClienteMensajeria {
     /**
      * Activa la cuenta del usuario enviando el token de confirmación de email.
      * Mapea a: GET /api/v1/auth/confirmar-email?token={token}
-     *
      * @param solicitud
      */
     @PostMapping("/api/v1/mensajeria/otp/generar")
     void generarCodigo(@RequestBody SolicitudGenerarOtp solicitud);
+    
+    /**
+     * Valida un código de recuperación y devuelve el UUID del usuario.
+     * Mapea al GET que creamos en el Controlador de Mensajería.
+     * @param codigo
+     * @return 
+     */
+    @GetMapping("/api/v1/mensajeria/otp/validar-recuperacion")
+    UUID validarCodigoYObtenerUsuario(@RequestParam("codigo") String codigo);
 }
