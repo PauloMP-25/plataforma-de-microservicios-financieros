24d0308b9c5328b5e5a765dd9ddb33cd168df82a
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-05-02 14:44:45 -0500

    módulo de gestión de presupuestos

---
+++ estructura-frontend/frontend/src/app/features/presupuestos/pages/presupuestos-page/presupuestos-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-presupuestos-page',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './presupuestos-page.html',
+  styleUrls: ['./presupuestos-page.scss'],
+})
+export class PresupuestosPage {
+
+}
