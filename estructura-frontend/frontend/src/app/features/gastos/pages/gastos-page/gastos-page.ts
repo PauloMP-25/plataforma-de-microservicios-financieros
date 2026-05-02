b615c456079a3ceae3299564082f1e7eec366eee
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-05-02 14:40:02 -0500

    módulo de gestión de gastos

---
+++ estructura-frontend/frontend/src/app/features/gastos/pages/gastos-page/gastos-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gastos-page',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './gastos-page.html',
+  styleUrl: './gastos-page.scss',
+})
+export class GastosPage {
+
+}
