fd9749d56b8358562c95b781685dade4c3032f1e
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-05-08 23:56:36 -0500

    Agregacion de  JwtInterceptor

--- estructura-frontend/frontend/src/app/app.config.ts
+++ estructura-frontend/frontend/src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './core/services/Jwt.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])) 
   ]
 };
