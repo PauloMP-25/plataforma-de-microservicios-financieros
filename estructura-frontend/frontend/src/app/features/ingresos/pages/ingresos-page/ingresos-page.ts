018dde96c210b607df699f1cfbc947e8ee41fc0b
Gabriel <gcarazasvelasquez@gmail.com>
2026-05-29 16:34:09 -0500

    feat: maquetación  clara de ingresos

--- estructura-frontend/frontend/src/app/features/ingresos/pages/ingresos-page/ingresos-page.ts
+++ estructura-frontend/frontend/src/app/features/ingresos/pages/ingresos-page/ingresos-page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-ingresos-page',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './ingresos-page.html',
   styleUrl: './ingresos-page.scss',
 })
 export class IngresosPage {
+  
+  // Propiedad para controlar si el modal se muestra o se oculta
+  isModalAbierto: boolean = false;
+
+  // Método para abrir el modal al dar click en "Registrar Ingreso"
+  abrirModal(): void {
+    this.isModalAbierto = true;
+  }
+
+  // Método para cerrar el modal al dar click en "Descartar" o la "X"
+  cerrarModal(): void {
+    this.isModalAbierto = false;
+  }
 
 }
